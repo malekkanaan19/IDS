8f4b873d0685368075261e0b2105e69e4de20c26
malekkanaan19 <125881036+malekkanaan19@users.noreply.github.com>
2023-06-25 23:59:12 +0300

    Create CompanyResources.cs

---
+++ WebAPI/Resources/CompanyResources.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_API.Resources
+{
+    public class CompanyResources
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
