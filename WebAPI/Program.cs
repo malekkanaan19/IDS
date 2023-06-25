da32cc364f39ae46621c32d4d35875d4ca4996b6
malekkanaan19 <125881036+malekkanaan19@users.noreply.github.com>
2023-06-26 00:07:40 +0300

    Create Program.cs

---
+++ WebAPI/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Custom method - replace with your own logic
+app.Use(async (context, next) =>
+{
+    // Your custom logic goes here
+    await next.Invoke();
+});
+
+app.Run();
